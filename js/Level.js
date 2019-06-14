352dc701b1e0b7c247a516d0b95da7d8e6839a78
Maxtanie Petit Dol <maxtaniepetitdol@outlook.fr>
2019-06-14 12:34:12 +0200

    PetsBoxes_Game

---
+++ js/Level.js
@@ -0,0 +1,129 @@
+export default class Level {
+  constructor(level, animal) {
+    // level must start at 0 and be incremented over time
+    this.animalToFind = animal;
+    this.baseBoxes = 4;
+    this.baseToFind = 2;
+    this.levelName = `Level-0${level + 1}`;
+    this.boxCount = this.baseBoxes + level * 2;
+    this.lives = level < 3 ? 3 : 5;
+    this.toFind = this.baseToFind + level * 1;
+    this.found = 0;
+    this.errMsg = "err-msg";
+    this.heartLife = "/img/heart-life.png";
+    this.boxClosed = "/img/box-closed.png";
+    this.boxEmpty = "/img/box-open.png";
+    this.boxCat = "/img/box-cat.png";
+    this.boxDog = "/img/box-dog.png";
+    this.boxLion = "/img/box-lion.png";
+    this.boxButterfly = "/img/box-butterfly.png";
+    this.petsTab = [
+      this.boxCat,
+      this.boxDog,
+      this.boxLion,
+      this.boxButterfly,
+      this.boxEmpty
+    ];
+  }
+
+  isFinished() {
+    return this.found === this.toFind;
+  }
+
+  drawBoxes() {
+    // console.log(this.animalToFind);
+    let output = "";
+
+    const animals = {
+      cat: this.petsTab[0],
+      dog: this.petsTab[1],
+      lion: this.petsTab[2],
+      butterfly: this.petsTab[3]
+    };
+
+    const levelImages = [];
+    const levelAnimalImage = animals[this.animalToFind];
+    const remainder = this.boxCount - this.toFind;
+    var counter = 0;
+
+    while (counter < this.toFind) {
+      levelImages.push(levelAnimalImage);
+      counter++;
+    }
+
+    // console.log("this.animalToFind", this.animalToFind);
+    // console.log("levelAnimalImage", levelAnimalImage);
+    // console.log("levelAnimalImage", levelAnimalImage);
+    // console.log("levelImages", levelImages);
+
+    counter = 0;
+
+    while (counter < remainder) {
+      const rand = Math.floor(Math.random() * this.petsTab.length);
+      const img = this.petsTab[rand];
+      if (img && img !== levelAnimalImage) {
+        levelImages.push(img);
+        counter++;
+      }
+    }
+
+    const shuffle = _.shuffle(levelImages);
+
+    function extractTarget(str) {
+      const match = str.match(/-|\w+|.png/g);
+      return match[match.length - 2];
+    }
+
+    // console.log("----------------------------");
+    // console.log("levelImages", levelImages);
+    // console.log("shuffeld", shuffle);
+
+    for (let i = 0; i < shuffle.length; i++) {
+      const attr = extractTarget(shuffle[i]);
+      output += `<img id="box-${i +
+        1}" class="box" data-box-content="${attr}" src="/img/box-closed.png" />`;
+    }
+    return output;
+  }
+
+  loseLife() {
+    this.lives--;
+  }
+
+  petsToFind(attr) {
+    return `${this.levelName} you have <b>${this.toFind}</b> <b>${
+      this.animalToFind
+    }s</b> to find.`;
+  }
+
+  drawLives() {
+    let output = "";
+    for (var i = 0; i < this.lives; i++) {
+      output += `
+        <img src="${this.heartLife}" class="circle-life" />
+      `;
+    }
+    return output;
+  }
+}
+/*errMessage() {
+    var blocMsg = document.getElementById(this.errMsg);
+    var elem = document.getElementsByClassName("level");
+    elem.onclick = function() {
+      if (this.found == 0) {
+        blocMsg.innerHTML = `You can't access to the ${elem.id}
+        without finish the level-1`;
+      } else {
+        return console.log(`Welcome ${this.name}`);
+      }
+    };
+  }
+
+  nextLevel() {
+    if (this.found !== this.toFind) {
+      alert("you must to find the box card");
+    } else {
+      alert("You have x to find");
+    }
+  }
+*/
---
+++ js/game.js
@@ -0,0 +1,113 @@
+import Level from "./Level.js";
+
+window.onscroll = function() {
+  console.log(this.scrollTop);
+};
+
+console.log(window.scrollTop);
+
+// Declaration of my global variables
+var currentLevelNumber = -1;
+var currentLevel = null;
+
+const btnStart = document.getElementById("start_game");
+const boxGameOver = document.getElementById("game-over");
+const boardGame = document.getElementById("game_board");
+var blocImgRandom = document.getElementsByClassName("bloc-img");
+var randomSelect = document.getElementById("random-img-select");
+var level = document.getElementsByClassName("level");
+const sectionBoxes = document.getElementById("section-boxes");
+const playerLifes = document.getElementById("player-lifes");
+const numberToFind = document.querySelector("#numberToFind");
+var chosenAnimal;
+
+function hideStartButton() {
+  btnStart.classList.toggle("is-hidden");
+}
+
+function randomBloc(val) {
+  const previous = document.querySelector(".bloc-img.is-active");
+  const blocImgRand = val[Math.floor(Math.random() * val.length)];
+  const disAttr = blocImgRand.querySelector("img").getAttribute("alt");
+  if (previous) previous.classList.remove("is-active");
+  blocImgRand.classList.add("is-active");
+  // displayAttribute(disAttr);
+  return blocImgRand.querySelector("img").alt.toLowerCase();
+}
+
+// function displayAttribute(attr) {
+//   randomSelect.innerHTML = `You must find the <b class="color-purple">${attr}</b> in the box.`;
+// }
+
+function gameOver() {
+  currentLevelNumber = -1;
+  hideStartButton();
+  boardGame.classList.add("is-hidden");
+  boxGameOver.classList.remove("is-hidden");
+}
+
+function openBox(evt) {
+  const img = evt.target || evt.srcElement;
+  const target = img.getAttribute("data-box-content");
+  img.src = `/img/box-${target}.png`;
+
+  if (target === chosenAnimal && target !== null) {
+    currentLevel.found++;
+    if (currentLevel.isFinished()) {
+      chosenAnimal = randomBloc(blocImgRandom);
+      setTimeout(function() {
+        setNextLevel(chosenAnimal);
+      }, 1500);
+    }
+  } else {
+    currentLevel.loseLife();
+    img.classList.add("none");
+    if (currentLevel.lives >= 1)
+      playerLifes.removeChild(playerLifes.children[0]);
+    else gameOver();
+  }
+  img.classList.add("opened");
+}
+
+function listenBoxClicks(boxes) {
+  boxes.forEach(box => {
+    box.onclick = openBox;
+  });
+}
+
+function $(sCSS) {
+  const tmp = document.querySelectorAll(sCSS);
+  return tmp.length === 1 ? tmp[0] : tmp;
+}
+
+function setNextLevel(animal, isRestart) {
+  currentLevelNumber++;
+  currentLevel = new Level(currentLevelNumber, animal);
+  if (currentLevelNumber > 0 || currentLevel < 5) {
+    const activeLevelItem = $(".level.is-active");
+    const nextActiveLevel = activeLevelItem.nextElementSibling;
+    activeLevelItem.classList.remove("is-active");
+    nextActiveLevel.classList.add("is-active");
+  }
+  sectionBoxes.innerHTML = currentLevel.drawBoxes();
+  //console.log("Current-level", currentLevel.levelName);
+  const boxes = document.querySelectorAll(".box");
+  listenBoxClicks(boxes);
+  playerLifes.innerHTML = currentLevel.drawLives();
+  numberToFind.innerHTML = currentLevel.petsToFind();
+  //
+}
+
+function startGame() {
+  chosenAnimal = randomBloc(blocImgRandom);
+  setNextLevel(chosenAnimal);
+  hideStartButton();
+  const firstLevelItem = document.querySelector(".level:first-of-type");
+  const previousActiveLevel = document.querySelector(".level.is-active");
+  if (previousActiveLevel) previousActiveLevel.classList.remove("is-active");
+  firstLevelItem.classList.add("is-active");
+  boxGameOver.classList.add("is-hidden");
+  boardGame.classList.remove("is-hidden");
+}
+
+btnStart.onclick = startGame;
